9d57dc388e101888f7f501cb71302c2c58ebaa5b
pratham-ak2004 <pratham20442@gmail.com>
2024-02-21 00:29:25 +0530

    feat: contact route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,13 +11,16 @@
 		<h1 class="text-4xl mx-auto mt-6"><b>real-incognito</b></h1>
 	</div>
 	<Separator class="h-1 bg-[#ed613d]"/>
-	
+	<div class="w-full h-auto p-6 mt-4 flex flex-col items-center gap-6">
+		<img src="https://picsum.photos/200/300" class="w-full mx-auto max-w-4xl aspect-video object-cover rounded-md" alt="">
+		<div class="w-full max-w-4xl flex flex-col lg:items-start items-center">
+			<div class="text-justify lg:text-left">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui voluptatibus sint maiores quaerat quisquam recusandae atque voluptas laborum dolore, praesentium voluptate distinctio earum eos quasi. Animi, dolore. Temporibus, eos voluptate.
+			</div>
+			<Button class="mt-4">click me</Button>
+		</div>
+	</div>
 </main>
 
 <style>
-	#home-background {
-		background-image: url('/images/home-page-background.jpg');
-		background-size: cover;
-		background-position: center;
-	}
 </style>
