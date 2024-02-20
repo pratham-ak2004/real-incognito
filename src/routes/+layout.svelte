d2b8a9b010da3c577bd9ff4864715d9c2995b52c
Prathwik <64214685+prathwik0@users.noreply.github.com>
2024-02-20 16:45:50 +0530

    feat: example routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import SiteHeader from '$lib/components/site-header.svelte';
+	import '../app.pcss';
+</script>
+
+<div class="relative flex min-h-screen flex-col" id="page">
+	<SiteHeader />
+	<div class="flex-1">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<h1>Hello! from Real Incognito</h1>
