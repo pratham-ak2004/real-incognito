caa5b953ae74f6cf336d604836301567d9320a0e
pratham-ak2004 <pratham20442@gmail.com>
2024-02-21 02:14:44 +0530

    bug: fixed contact UI

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,25 +3,88 @@
 </script>
 
 <main>
-    <div class="w-full h-screen">
-		<!-- img , below link to documentation with button -->
-		<div class="flex md:flex-row flex-col max-w-screen-lg mx-auto mt-6 gap-8 p-8">
-			<img src="/images/kausthubh.png" class="aspect-square max-w-80 w-60 mx-auto rounded-full object-cover" alt="">
-			<div class="text-justify md:text-left text-lg font-medium my-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore impedit ipsam eius illum reprehenderit rerum consequatur ex harum fugiat placeat, incidunt accusantium molestiae quis delectus eveniet porro similique? Aspernatur omnis consequuntur nisi eius aliquam quibusdam vel eligendi laborum repudiandae temporibus iure nihil voluptate perspiciatis, nulla dolorem officia quidem minima earum.</div>
+    <!-- <div class="w-full h-screen">
+		<div class="flex md:flex-row flex-col max-w-screen-lg mx-auto mt-6 gap-8 p-8 justify-around items-center">
+			<img src="/images/kausthubh.png" class="aspect-square max-w-80 w-60 rounded-full object-cover md:basis-1/2" alt="">
+			<div class="text-justify md:text-left text-lg font-medium my-auto items-center md:basis-1/2 self-start">
+				<h1 class="font-bold text-3xl">Kausthubh</h1>
+				<h2>Department of AIML</h2>
+				<h2>"LIFE COULD BE DREAM"</h2>
+			</div>
 		</div>
-		<div class="flex md:flex-row-reverse flex-col max-w-screen-lg mx-auto mt-6 gap-8 p-8">
-			<img src="https://picsum.photos/200/300" class="aspect-square max-w-80 w-60 object-cover mx-auto rounded-full" alt="">
-			<div class="text-justify md:text-left text-lg font-medium my-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore impedit ipsam eius illum reprehenderit rerum consequatur ex harum fugiat placeat, incidunt accusantium molestiae quis delectus eveniet porro similique? Aspernatur omnis consequuntur nisi eius aliquam quibusdam vel eligendi laborum repudiandae temporibus iure nihil voluptate perspiciatis, nulla dolorem officia quidem minima earum.</div>
+		<div class="flex md:flex-row-reverse flex-col max-w-screen-lg mx-auto mt-6 gap-8 p-8  items-center justify-end">
+			<img src="https://picsum.photos/200/300" class="aspect-square max-w-80 w-60 rounded-full object-cover md:basis-1/2 " alt="">
+			<div class="text-justify md:text-left text-lg font-medium my-auto items-center md:basis-1/2 self-end">
+				<h1 class="font-bold text-3xl">Kausthubh</h1>
+				<h2>Department of AIML</h2>
+				<h2>"LIFE COULD BE DREAM"</h2>
+			</div>
 		</div>
-		<div class="flex md:flex-row flex-col max-w-screen-lg mx-auto mt-6 gap-8 p-8">
-			<img src="/images/pratham.png" class="aspect-square max-w-80 w-60 object-cover mx-auto rounded-full" alt="">
-			<div class="text-justify md:text-left text-lg font-medium my-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore impedit ipsam eius illum reprehenderit rerum consequatur ex harum fugiat placeat, incidunt accusantium molestiae quis delectus eveniet porro similique? Aspernatur omnis consequuntur nisi eius aliquam quibusdam vel eligendi laborum repudiandae temporibus iure nihil voluptate perspiciatis, nulla dolorem officia quidem minima earum.</div>
+		<div class="flex md:flex-row flex-col max-w-screen-lg mx-auto mt-6 gap-8 p-8 justify-around items-center">
+			<img src="/images/pratham.png" class="aspect-square max-w-80 w-60 rounded-full object-cover md:basis-1/2" alt="">
+			<div class="text-justify md:text-left text-lg font-medium my-auto items-center md:basis-1/2">
+				<h1 class="font-bold text-3xl">Kausthubh</h1>
+				<h2>Department of AIML</h2>
+				<h2>"LIFE COULD BE DREAM"</h2>
+			</div>
 		</div>
-		<div class="flex md:flex-row-reverse flex-col max-w-screen-lg mx-auto mt-6 gap-8 p-8">
-			<img src="https://picsum.photos/200/300" class="aspect-square max-w-80 w-60 object-cover mx-auto rounded-full" alt="">
-			<div class="text-justify md:text-left text-lg font-medium my-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore impedit ipsam eius illum reprehenderit rerum consequatur ex harum fugiat placeat, incidunt accusantium molestiae quis delectus eveniet porro similique? Aspernatur omnis consequuntur nisi eius aliquam quibusdam vel eligendi laborum repudiandae temporibus iure nihil voluptate perspiciatis, nulla dolorem officia quidem minima earum.</div>
+		<div class="flex md:flex-row-reverse flex-col max-w-screen-lg mx-auto mt-6 gap-8 p-8  items-center">
+			<img src="https://picsum.photos/200/300" class="aspect-square max-w-80 w-60 rounded-full object-cover md:basis-1/2" alt="">
+			<div class="text-justify md:text-left text-lg font-medium my-auto items-center md:basis-1/2 self-end">
+				<h1 class="font-bold text-3xl">Kausthubh</h1>
+				<h2>Department of AIML</h2>
+				<h2>"LIFE COULD BE DREAM"</h2>
+			</div>
+		</div>
+	</div> -->
+
+	<div class="h-full w-full gap-4 flex flex-col py-10 gap-y-6">
+		<div class="w-full flex items-center md:justify-start flex-col md:flex-row max-w-4xl mx-auto gap-6">
+			<img src="/images/kausthubh.png" class="aspect-square max-w-80 w-60 rounded-full object-cover" alt="">
+			<div class="md:text-left text-lg font-medium my-auto md:self-start text-center">
+				<h1 class="font-bold text-3xl">Kausthubh</h1>
+				<h2>Department of AIML</h2>
+				<h2>"LIFE COULD BE DREAM"</h2>
+			</div>
+		</div>
+		<div class="w-full flex items-center md:justify-end flex-col-reverse md:flex-row max-w-4xl mx-auto gap-6">
+			<div class="md:text-left text-lg font-medium my-auto md:self-start text-center">
+				<h1 class="font-bold text-3xl">Pratwik</h1>
+				<h2>Department of AIML</h2>
+				<h2>"NEVER GONNA GIVE YOU UP"</h2>
+			</div>
+			<img src="/images/temp-img.png" class="aspect-square max-w-80 w-60 rounded-full object-cover" alt="">
+		</div>
+		<div class="w-full flex items-center md:justify-start flex-col md:flex-row max-w-4xl mx-auto gap-6">
+			<img src="/images/pratham.png" class="aspect-square max-w-80 w-60 rounded-full object-cover" alt="">
+			<div class="md:text-left text-lg font-medium my-auto md:self-start text-center">
+				<h1 class="font-bold text-3xl">Pratham</h1>
+				<h2>Department of AIML</h2>
+				<h2>"JUST KEEP IT FLOWING"</h2>
+			</div>
+		</div>
+		<div class="w-full flex items-center md:justify-end flex-col-reverse md:flex-row max-w-4xl mx-auto gap-6">
+			<div class="md:text-left text-lg font-medium my-auto md:self-start text-center">
+				<h1 class="font-bold text-3xl">Shawn</h1>
+				<h2>Department of AIML</h2>
+				<h2>"LIFE COULD BE DREAM"</h2>
+			</div>
+			<img src="/images/shawn.png" class="aspect-square max-w-80 w-60 rounded-full object-cover" alt="">
 		</div>
 	</div>
+
+	<!-- <div class="w-full h-screen p-4 flex flex-row">
+		<div class="basis-1/2 flex flex-col">
+			<img src="/images/kausthubh.png" class="rounded-full aspect-square object-cover" alt="">
+		</div>
+		<div class="basis-1/2">
+			<div class="text-justify md:text-left text-lg font-medium my-auto">
+				<h1 class="font-bold text-3xl">Kausthubh</h1>
+				<h2>Department of AIML</h2>
+				<h2>"LIFE COULD BE DREAM"</h2>
+			</div>
+		</div>
+	</div> -->
 </main>
 
 <style>
